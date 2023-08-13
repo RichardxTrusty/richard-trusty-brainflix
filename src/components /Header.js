c490eb06f0802d14c2f9027dad26cc1e143ae0fb
RichardXTrusty <ratrusty92@gmail.com>
2023-08-13 08:21:20 -0400

    Created header component

---
+++ src/components /Header.js
@@ -0,0 +1,34 @@
+import Logo from "../assets/Logo/BrainFlix-logo.svg";
+import Search from "../assets/Icons/search.svg";
+import Upload from "../assets/Icons/upload.svg";
+
+function Header(props) {
+  return (
+    <header className="header">
+      <img className="header__logo" src={Logo} alt="brainflix logo"></img>
+      <form className="header__form">
+        <div className="header__searchWrapper">
+          <input
+            className="header__search"
+            type="search"
+            placeholder="Search"></input>
+          <img
+            className="header__searchImg"
+            src={Search}
+            alt="brainflix seaerch icon"></img>
+        </div>
+        <div className="header__form-bottom">
+          <button className="header__button" type="sutmit">
+            <img
+              className="header__uploadImg"
+              src={Upload}
+              alt="brainflix seaerch icon"></img>
+            UPLOAD
+          </button>
+          <div className="header__mohan"></div>
+        </div>
+      </form>
+    </header>
+  );
+}
+export default Header;
